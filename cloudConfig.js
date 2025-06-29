129218b4f7575f9bef43128d57f6d2eb6e39c929
Ankur Dutta <ankurxcode@gmail.com>
2025-06-29 15:17:32 +0530

    configure change in cloudConfig.js file

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,21 +1,20 @@
 const cloudinary = require('cloudinary');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
+// Cloudinary Config
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-
-
+// Multer Storage for Cloudinary
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'wanderland_dev',
-    allowedFormats: ['jpeg', 'png', 'jpg'], 
-     format: 'jpg',     
-  },
+    allowed_formats: ['jpeg', 'png', 'jpg'],
+  }
 });
 
-module.exports={cloudinary,storage}
+module.exports = { cloudinary, storage };
